aa459e01444f5d8196c46f34596bf7226242ba97
tejaswiniBoyapati <teju3663@gmail.com>
2022-08-09 21:32:36 +0530

    Create Intro.py

---
+++ Intro.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  9 12:51:17 2022
+
+@author: teju3
+"""
+
+print("Hello World")
+print('HI heyy')
+### Variables
+
+a = 10
+b = 20
+a + b
+print(a+b)
+
+###type
+
+print(type('c'))
+c=100
+type(c)
